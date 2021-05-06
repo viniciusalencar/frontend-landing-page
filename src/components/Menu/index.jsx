4416c287031ac72ca6847e8c914983ea5e0b06a1
viniciusalencar <viniciusalencar176@gmail.com>
2021-05-05 19:35:31 -0500

    Finish a component Menu with storybook and tests

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -3,18 +3,19 @@ import * as Styled from './styles';
 import { SectionContainer } from '../SectionContainer';
 import { LogoLink } from '../LogoLink';
 import { NavLinks } from '../NavLinks';
-import { Menu as MenuIcon } from '@styled-icons/meterial-outlined/Menu';
-import { Close as CloseIcon } from '@styled-icons/meterial-outlined/Close';
+import { Menu as MenuIcon } from '@styled-icons/material-outlined/Menu';
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
 import { useState } from 'react';
 
 export const Menu = ({ links = [], logoData }) => {
   const [visible, setVisible] = useState(false);
+
   return (
     <>
       <Styled.Button
         visible={visible}
         onClick={() => setVisible(true)}
-        aria-label="Open/Close Menu"
+        aria-label="Open/Close menu"
       >
         {visible ? (
           <CloseIcon aria-label="Close menu" />
@@ -22,9 +23,9 @@ export const Menu = ({ links = [], logoData }) => {
           <MenuIcon aria-label="Open menu" />
         )}
       </Styled.Button>
-      <Styled.Container visible={visible}>
+      <Styled.Container visible={visible} onClick={() => setVisible(false)}>
         <SectionContainer>
-          <Styled.MenuContainer onClick={() => setVisible(false)}>
+          <Styled.MenuContainer>
             <LogoLink {...logoData} />
             <NavLinks links={links} />
           </Styled.MenuContainer>
@@ -36,5 +37,5 @@ export const Menu = ({ links = [], logoData }) => {
 
 Menu.propTypes = {
   ...NavLinks.propTypes,
-  logoData: P.shape(LogoLink.propTypes).isRequired(),
+  logoData: P.shape(LogoLink.propTypes).isRequired,
 };
